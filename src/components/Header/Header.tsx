c4acec6f5310a341326670d2ceb8127312af41fd
Murad <mmuradilh@gmail.com>
2023-11-25 13:54:02 +0400

    Some components created

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export function Header(){
+    return(
+        <div className="Header-container">
+            <p className="agent-name">Lorem Impus</p>
+            <button className="Login-button">Login</button>
+            <button className="Signup-button">Sign Up</button>
+        </div>
+    );
+}
