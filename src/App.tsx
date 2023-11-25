16e414478ca6973b2becbdfd68475de41a0c6a35
Murad <mmuradilh@gmail.com>
2023-11-25 16:43:00 +0400

    MainPage created

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
+import { Footer } from './components/MainPage/Footer/Footer';
 import { MainPage } from './components/MainPage/MainPage';
 
 function App() {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,15 @@ import "./Header.scss";
 export function Header() {
   return (
     <div className="Header-container">
-      <p className="agent-name">Lorem Impus</p>
+      <div className="TravelAgent-container">
+        <span className="agent-name">TRAVEL </span>
+        <img src="" />
+        <span className="agent-name">AGENT</span>
+      </div>
       <div className="Button-container">
-        <button className="Home-button ">Home</button>
-        <button className="Login-button ">Login</button>
-        <button className="Signup-button ">Sign Up</button>
+        <button className="Home-button">Home</button>
+        <button className="Login-button">Login</button>
+        <button className="Signup-button">Sign Up</button>
       </div>
     </div>
   );
