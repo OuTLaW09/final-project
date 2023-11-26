0930463635b497a3840fc2f704645b4bd20ec469
Murad <mmuradilh@gmail.com>
2023-11-27 00:05:09 +0400

    sdfghj

--- src/components/DefaultPage/DefaultPage.tsx
+++ src/components/DefaultPage/DefaultPage.tsx
@@ -3,6 +3,9 @@ import backgroundimage from "../../assets/Images/EnteringImage.jpg"
 export const DefaultPage=()=>{
     return (
         <div className="DefaultPage-container">
+            <div className="Tryingdiv">
+                <p>Salam</p>
+            </div>
         </div>
     )
 }
