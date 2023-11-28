b6bef7cb7d184fc3517742c66d1bd0900de90692
Murad <mmuradilh@gmail.com>
2023-11-28 12:36:35 +0400

    Carousel array added

---
+++ src/models/CarouselObject.ts
@@ -0,0 +1,47 @@
+
+
+export const cities = [
+  {
+    imageUrl: 'image',
+    seasons: ["spring, summer"],
+    rating: 9 / 10,
+    packages: ["weekly, monthly"],
+    description: "Lorem Impus",
+  },
+  {
+    imageUrl: 'image',
+    seasons: "all seasons",
+    rating: 10 / 10,
+    packages: ["weekly, monthly"],
+    description: "Lorem Impus",
+  },
+  {
+    imageUrl: 'image',
+    seasons: ["spring, summer"],
+    rating: 10 / 10,
+    packages: ["weekly, monthly"],
+    description: "Lorem Impus",
+  },
+  {
+    imageUrl: 'image',
+    seasons: "summer",
+    rating: 8 / 10,
+    packages: ["weekly, monthly"],
+    description: "Lorem Impus",
+  },
+  {
+    imageUrl: 'image',
+    seasons: "all seasons",
+    rating: 9 / 10,
+    packages: ["weekly, monthly"],
+    description: "Lorem Impus",
+  },
+  {
+    imageUrl: 'image',
+    seasons: "all seasons",
+    rating: 8 / 10,
+    packages: ["weekly, monthly"],
+    description: "Lorem Impus",
+  },
+];
+
