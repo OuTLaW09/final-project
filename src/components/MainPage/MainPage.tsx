8e0e4a6253229784d5e3b5e0b1ebb57907ddc7a9
Murad <mmuradilh@gmail.com>
2023-12-05 01:37:15 +0400

    Datepicker is ready

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,8 +1,47 @@
+import "./Mainpage.scss";
 import { Footer } from "../Footer/Footer";
 import { CarouselPage } from "../CarouselPage/CarouselPage";
 import { HeroPage } from "../HeroPage/HeroPage";
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import type { DatePickerProps } from 'antd';
+import { DatePicker, Space } from 'antd';
 
-import "./Mainpage.scss";
+dayjs.extend(customParseFormat);
+
+const { RangePicker } = DatePicker;
+
+const dateFormat = 'YYYY/MM/DD';
+const weekFormat = 'MM/DD';
+const monthFormat = 'YYYY/MM';
+
+/** Manually entering any of the following formats will perform date parsing */
+const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
+
+const customFormat: DatePickerProps['format'] = (value) =>
+  `custom format: ${value.format(dateFormat)}`;
+
+const customWeekStartEndFormat: DatePickerProps['format'] = (value) =>
+  `${dayjs(value).startOf('week').format(weekFormat)} ~ ${dayjs(value)
+    .endOf('week')
+    .format(weekFormat)}`;
+
+const App: React.FC = () => (
+  <Space direction="vertical" size={12}>
+    <DatePicker defaultValue={dayjs('2015/01/01', dateFormat)} format={dateFormat} />
+    <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} />
+    <DatePicker defaultValue={dayjs('2015/01', monthFormat)} format={monthFormat} picker="month" />
+    <DatePicker defaultValue={dayjs()} format={customWeekStartEndFormat} picker="week" />
+    <RangePicker
+      defaultValue={[dayjs('2015/01/01', dateFormat), dayjs('2015/01/01', dateFormat)]}
+      format={dateFormat}
+    />
+    <DatePicker defaultValue={dayjs('2015/01/01', dateFormat)} format={customFormat} />
+  </Space>
+);
+
+export default App;
+
 export function Mainpage() {
   return (
     <div className="main-page-container">
@@ -46,7 +85,8 @@ export function Mainpage() {
             <div className="rotation-container">
               <input type="text" placeholder="Where From?"/>
               <input type="text" placeholder="Where To?"/>
-              <input type="text" placeholder="Departure"/>
+              <DatePicker picker="date"/>
+              
               <input type="text" placeholder="Where To?"/>
             </div>
           </div>
