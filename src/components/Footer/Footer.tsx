6241dd02164f307447961aff4d63deb74e0995d0
Zeyn03 <zeynebemiraslanov2003@gmail.com>
2023-12-04 22:38:40 +0400

    give some shapes to pages

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import FooterTopImage from '../../assets/Images/footerImage.png';
 import BackToTopImage from '../../assets/Images/up-arrrows.png';
 import { Link } from 'react-router-dom';
 
-export const Footer =( )=>{
+export const Footer =()=>{
     return(
         <div className="footer-main">
             <div className="top-of-footer">
@@ -17,6 +17,19 @@ export const Footer =( )=>{
 
             </div>
             <div className="bottom-of-footer">
+                <div className='content-of-bottom-footer'>
+                    <div className='header-of-bottom'>
+                        <div>Travel Agency</div>
+                        <div>Lorem Ipsum jfjfjfjf</div>
+                        <div className="get-in-touch">Get in touch</div>
+                    </div>
+                    <div className="middle-of-bottom-footer">
+                        <a href="">Home</a>
+                        <a href="">About us</a>
+                        <a href="">Follow us</a>
+                    </div>
+
+                </div>
 
             </div>
 
