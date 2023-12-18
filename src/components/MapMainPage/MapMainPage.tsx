a1b81db68f91c91c08814b99fe70454ad07bc222
Zeyn03 <zeynebemiraslanov2003@gmail.com>
2023-12-19 00:56:04 +0400

    test

--- src/components/MapMainPage/MapMainPage.tsx
+++ src/components/MapMainPage/MapMainPage.tsx
@@ -1,41 +1,87 @@
 import './MapMainPage.scss';
 import { MapContainer, Marker, Polyline, Popup, TileLayer } from 'react-leaflet';
-import { DepartureArray } from '../MainPage/MainPage';
-import L from 'leaflet';
-const markerIcon = new L.Icon({
-  iconUrl: require('../../assets/Images/marker.png'),
-  iconSize: [45, 45],
-  iconAnchor: [17, 46],
-  popupAnchor: [0, -46],
-});
+import { DepartureArray, DepartureArrayName,themeId,docId } from '../MainPage/MainPage';
+import L, { DivIcon } from 'leaflet';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const createCustomIcon = (name: string) => {
+  return L.divIcon({
+    className: 'custom-marker',
+    html: 
+      `<div class='inside-marker'>
+        <div class= "circle-side">
+          <p>time</p>
+        </div>
+        <div class="city-name">${name}</div>
+      </div> `
+    ,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+};
 
 export const MapMainPage = () => {
-  console.log(DepartureArray);
-  const departureArrayForPolyline: any = [...DepartureArray];
+  const { signature } = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      let response: Response;
+
+      do {
+        response = await fetch(`https://api.eightydays.me/api/v3/tour/get/${signature}`);
+        await new Promise((resolve, reject) => setTimeout(() => resolve(true), 1000));
+      } while (response.status === 204);
+
+      const data = await response.json();
+      // set state here
+      console.log(data);
+    };
+
+    fetchData();
+  }, [signature]);
+
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const departureArrayForPolyline: any[] = [...DepartureArray];
   departureArrayForPolyline.push(DepartureArray[0]);
-  
-  console.log('departureArrayForPolyline', departureArrayForPolyline);
+
   return (
     <div className="map">
+      {selectedCount !== null && (
       <MapContainer center={[40.409264, 49.867092]} zoom={2}>
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>'
         />
-        {DepartureArray.map((cityLoc) => {
-          console.log('cityLoc', cityLoc);
+        {DepartureArray.map((cityLoc, index) => {
           return (
             <>
-              <Marker position={cityLoc} icon={markerIcon}>
-                <Popup>
-                  <b>city</b>
-                </Popup>
+              <Marker position={cityLoc} icon={createCustomIcon(DepartureArrayName[index])}>
+                <Popup>{DepartureArrayName[index]}</Popup>;
               </Marker>
-              <Polyline positions={departureArrayForPolyline} />
+              return(
+              <>
+                {departureArrayForPolyline.map(
+                  (cityLoc, index, array) =>
+                    index < array.length - 1 && <Polyline key={index} positions={[cityLoc, array[index + 1]]} color={getRandomColor()} />,
+                )}
+              </>
+              )
             </>
           );
         })}
       </MapContainer>
+      <div className='ticket'>
+          <p>Test</p>
+      </div>
+  
     </div>
   );
 };
